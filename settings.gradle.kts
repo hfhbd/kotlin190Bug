fa214770ed8615135c34f7b675dcb9ea99d2ef66
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-05-23 13:22:39 +0200

    Add build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
 rootProject.name = "untitled"
+
+plugins {
+    id("com.gradle.enterprise") version "3.13.2"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
